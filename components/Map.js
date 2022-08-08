927e56e3803ee145f1e93dad2cc3dc84023e8909
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-08 10:33:17 -0500

    added navigations card

--- components/Map.js
+++ components/Map.js
@@ -15,10 +15,23 @@ const Map = () => {
     initialRegion={{
       latitude: origin.location.lat,
       longitude: origin.location.lng,
-      latitudeDelta: 0.095,
+      latitudeDelta: 0.005,
       longitudeDelta: 0.005,
     }}
-  />
+  >
+
+      {origin?.location && (
+        <Marker 
+         coordinate={{ 
+          latitude: origin.location.lat, 
+          longitude: origin.location.lng,
+      }}
+        title="Origin"
+        description={origin.description}
+        identifier="origin"
+        />
+      )}
+    </MapView>
   );
 };
 
