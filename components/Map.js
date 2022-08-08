842ea4ecd6c787b17c24358a5d2f6a0deab1ca14
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-07 20:50:43 -0500

    map and origin calculations added

---
+++ components/Map.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView, {Marker} from 'react-native-maps';
+import tw from 'tailwind-react-native-classnames';
+import { selectOrigin } from "../slices/navSlice";
+import { useSelector } from "react-redux";
+
+const Map = () => {
+
+    const origin = useSelector(selectOrigin)
+  return (
+    <MapView
+    style={tw`flex-1`}
+    mapType="mutedStandard"
+    initialRegion={{
+      latitude: origin.location.lat,
+      longitude: origin.location.lng,
+      latitudeDelta: 0.095,
+      longitudeDelta: 0.005,
+    }}
+  />
+  );
+};
+
+export default Map
+
+const styles = StyleSheet.create({})
